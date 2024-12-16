e2f184f1a8e3e71693b08565258f1b3fc0867b87
Jung-Que <108854679+Jung-Que@users.noreply.github.com>
2024-12-16 16:33:38 +0900

    test

---
+++ jungDemo/src/main/java/com/example/jungDemo/JungDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.jungDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JungDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JungDemoApplication.class, args);
+	}
+
+}
